3e759955c9ee7161a0757fe0b607cc2f50ea39cf
Sandhya023 <yadavshndhya@gmail.com>
2018-04-08 20:38:47 +0530

    concatenation Strings

---
+++ Concate.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Solution{
+ public static void public static void main(String[] args) {
+ 
+	      int i=4;
+	      double d=4.0;
+	      String s="HackerRank";	
+	   Scanner scan=new Scanner(System.in);
+	      int j=12;
+	      double k= 4.0;
+	      String m="is the best place to learn and practice coding!";
+
+      int s1=i+j;
+           System.out.println(s1);
+      double s2= d+ k;
+           System.out.println(s2);
+      String l=s.concat(m);
+           System.out.println(l);
+            scan.close();
+        }
+    }
